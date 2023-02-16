27c199eb29ed8ff3a4f51f80bc16de957ae51b6b
barnadsv <barnadsv@gmail.com>
2023-02-16 00:59:53 -0300

    Voltando a config vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
+// import adapter from 'svelte-adapter-firebase';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,6 +12,7 @@ const config = {
 	],
 
 	kit: {
+		// adapter: adapter()
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
