55f3af6bb80a99cb6d455b0a95b606b863515304
barnadsv <barnadsv@gmail.com>
2023-03-18 23:54:56 -0300

    Flowbite funcionando

--- src/lib/components/landingpage/Hero.svelte
+++ src/lib/components/landingpage/Hero.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { goto } from '$app/navigation';
+	import { Alert } from 'flowbite-svelte';
+	import { Button } from 'flowbite-svelte';
 
 	const goToApp = () => {
 		goto('/app/install');
@@ -10,21 +12,18 @@
 	class="pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-secondary"
 >
 	<div class="md:flex-1 md:mr-10">
-		<h1 class="font-pt-serif text-5xl font-bold mb-7">
-			A headline for your
-			<span class="bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100%"> cool website </span>
-		</h1>
+		<Alert>
+			<h1 class="font-pt-serif text-5xl font-bold mb-7">
+				A headline for your
+				<span class="bg-underline1 bg-left-bottom bg-no-repeat pb-2 bg-100%"> cool website </span>
+			</h1>
+		</Alert>
 		<p class="font-pt-serif font-normal mb-7">
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum harum tempore consectetur
 			voluptas, cumque nobis laboriosam voluptatem.
 		</p>
 		<div class="font-montserrat">
-			<button
-				class="bg-black px-6 py-4 rounded-lg border-2 border-black border-solid text-white mr-2 mb-2"
-				on:click={goToApp}
-			>
-				Install app
-			</button>
+			<Button size="lg" color="dark" on:click={goToApp}>Install app</Button>
 		</div>
 	</div>
 	<div class="flex justify-around md:block mt-8 md:mt-0 md:flex-1">
